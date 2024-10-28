f8ab8529c8b85c026b9e3173ea2014c17b077439
PedroHBS822 <107572307+PedroHBS822@users.noreply.github.com>
2024-10-28 17:33:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 function calculateDays() {
-    const startDate = new Date(document.getElementById('start-date').value);
-    const endDate = new Date(document.getElementById('end-date').value);
+    const startDateInput = document.getElementById('start-date').value;
+    const endDateInput = document.getElementById('end-date').value;
     const businessDaysOnly = document.getElementById('business-days').checked;
+    const start = document.getElementById('start-date');
+    const end = document.getElementById('end-date');
+    const startError = document.getElementById('start-error');
+    const endError = document.getElementById('end-error');
 
-    if (isNaN(startDate) || isNaN(endDate)) {
-        document.getElementById('result').innerText = "Por favor, selecione ambas as datas.";
-        return;
+    const startDate = new Date(startDateInput);
+    const endDate = new Date(endDateInput);
+
+    // Limpa mensagens de erro
+    startError.style.display = 'none';
+    endError.style.display = 'none';
+
+    // Verifique se as datas são válidas
+    let valid = true;
+    if (isNaN(startDate.getTime())) {
+        start.style.borderColor = 'red';
+        startError.style.display = 'block';
+        startError.style.margin= 0;
+        startError.style.marginTop= '10px'; // Mostra a mensagem de erro
+        valid = false;
+    } else {
+        start.style.borderColor = '#7F00FF'; // Resetar a borda para a cor padrão se válido
+    }
+
+    if (isNaN(endDate.getTime())) {
+        end.style.borderColor = 'red';
+        endError.style.display = 'block';
+        endError.style.margin= 0;
+        endError.style.marginTop= '10px'; // Mostra a mensagem de erro
+        valid = false;
+    } else {
+        end.style.borderColor = '#7F00FF'; // Resetar a borda para a cor padrão se válido
+    }
+
+    if (!valid) {
+        return; // Se alguma data não for válida, saia da função
     }
 
     let totalDays = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
@@ -13,7 +45,7 @@ function calculateDays() {
 
     if (businessDaysOnly) {
         result = 0;
-        let currentDate = startDate;
+        let currentDate = new Date(startDate); // Clone a data inicial
         
         while (currentDate <= endDate) {
             const dayOfWeek = currentDate.getDay();
@@ -26,7 +58,6 @@ function calculateDays() {
 
     document.getElementById('result').innerText = `Total de dias: ${result}`;
 }
-
 
 document.getElementById('mobile-menu').addEventListener('click', function() {
     const navList = document.querySelector('.nav-list');
